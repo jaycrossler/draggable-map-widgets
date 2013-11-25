b7b8f2e08cfd2639074313f9a64580e6dc799119
Jay Crossler <jay.crossler@gmail.com>
2013-11-24 23:47:15 -0500

    Add support for opacity in data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app=window.app || {};
 app.background_map=null;
 app.mapManager=[];
+app.saveConfig=true;
 
 app.init=function(){
     app.addBackgroundMap();
@@ -14,7 +15,7 @@ app.init=function(){
 
     //TODO: Have a function that auto-builds menubars
     //TODO: Add function for adding map widgets and other type of widgets
-    //TODO: Add ability to change opacity of widgets
+    //TODO: Add UI ability to change opacity of widgets
     //TODO: Handle multiple layers per map widget
     //TODO: Build into a Django app
     //TODO: Make menu titlebars smaller
@@ -71,10 +72,46 @@ app.addWidgets=function(widget_list){
 
     var $holder = $("#widget_holder");
     $.each(widget_list,function(i,widget){
-        //TODO: if widget is already drawn, just modify position, not re-add
-        app.addWidget(widget,$holder);
+        if (app.isWidgetAlreadyDrawn(widget)){
+            app.updateWidget(widget);
+        } else {
+            app.addWidget(widget,$holder);
+        }
     });
+};
+app.isWidgetAlreadyDrawn=function(options){
+    var id = app.titleize(options.name);
+    var $w = $('#'+id);
+    return $w && $w.length;
+};
+app.updateWidget=function(options){
+    var widget = app.getWidget(options.name);
+    if (widget) {
+        widget.widget.$content.css({width:options.width,height:options.height});
+        if (options.minimized && options.minimized=="none"){
+            widget.widget.$content.css('display','none');
+        } else {
+            widget.widget.$content.css('display','block');
+        }
+        widget.widget.$holder.offset({top:options.top,left:options.left});
+        widget.widget.$titlebar.css({width:options.width+20});
+        if (options.opacity) {
+            widget.widget.$holder.fadeTo(100,options.opacity);
+        }
 
+        if (widget.map){
+            widget.map.updateSize();
+        }
+
+        if (widget.map && options.extents) {
+            var e = options.extents;
+            var bounds = [e.left, e.bottom, e.right, e.top];
+            widget.map.zoomToExtent(bounds);
+        }
+        if (widget.map && options.zoom) {
+            widget.map.zoomTo(options.zoom);
+        }
+    }
 };
 app.addWidget=function(options,$holder){
     options = app.lookupWidgetInfo(options);
@@ -97,7 +134,7 @@ app.addWidget=function(options,$holder){
     }
 
     var $content = $('<p class="well">').appendTo($widget_wrapper);
-    var id = options.divid || options.name.replace(/[ -0-9]/g,"");
+    var id = options.divid || app.titleize(options.name);
     var height = options.height || 200;
     var width = $content.parent().css('width')-20;
     $content
@@ -189,9 +226,10 @@ app.getConfig=function(notAsJSON){
         var width = parseInt($content.css('width'));
         var height = parseInt($content.css('height'));
         var top = parseInt(data.$holder.position().top);
-        var left = parseInt(data.$holder.position().left)-10;
+        var left = parseInt(data.$holder.position().left)+10;
         var minimized = $content.css('display');
-        var widget_info = {widget_id:data.widget_id, name:name, width:width, height:height, top:top, left:left, minimized:minimized};
+        var opacity = app.getOpacity(data.$holder);
+        var widget_info = {widget_id:data.widget_id, name:name, width:width, height:height, top:top, left:left, minimized:minimized, opacity:opacity};
 
         if (widget_holder.map && widget_holder.map.getExtent) {
             widget_info.extents = widget_holder.map.getExtent();
@@ -203,8 +241,10 @@ app.getConfig=function(notAsJSON){
     return notAsJSON ? config : JSON.stringify(config);
 };
 app.setCookie=function(){
-    var config = app.getConfig();
-    $.cookie('widget_config',config);
+    if (app.saveConfig) {
+        var config = app.getConfig();
+        $.cookie('widget_config',config);
+    }
 };
 app.getCookie=function(){
     var cookie = $.cookie('widget_config') || '{"status":"empty"}';
@@ -213,32 +253,41 @@ app.getCookie=function(){
 app.restoreConfigFromCookie=function(){
     var cookie = app.getCookie();
     if (cookie && cookie.widgets){
-        $.each(cookie.widgets,function(i,widget_info){
-
-            var widget = app.getWidget(widget_info.name);
-            if (widget) {
-                widget.widget.$content.css({width:widget_info.width,height:widget_info.height});
-                if (widget_info.minimized && widget_info.minimized=="none"){
-                    widget.widget.$content.css('display','none');
-                }
-                widget.widget.$holder.offset({top:widget_info.top,left:widget_info.left});
-                widget.widget.$titlebar.css({width:widget_info.width+20});
-
-                if (widget.map){
-                    widget.map.updateSize();
-                }
-
-                if (widget.map && widget_info.extents) {
-                    var e = widget_info.extents;
-                    var bounds = [e.left, e.bottom, e.right, e.top];
-                    widget.map.zoomToExtent(bounds);
-                }
-                if (widget.map && widget_info.zoom) {
-                    widget.map.zoomTo(widget_info.zoom);
-                }
-
-            }
-
-        });
+        app.addWidgets(cookie.widgets);
     }
+};
+app.restoreConfigFromString=function(configString){
+    configString = '{"widgets":[{"widget_id":"mini_map","name":"First Floor","opacity":1,"width":330,"height":200,"top":72,"left":40,"minimized":"block","extents":{"left":-77.044540606474,"bottom":38.893079020501,"right":-77.040785513854,"top":38.895439364435},"zoom":17},{"widget_id":"mini_map","name":"Second Floor","width":483,"height":283,"top":80,"left":425,"minimized":"block","extents":{"left":-77.045628432422,"bottom":38.89201954794,"right":-77.042930130154,"top":38.893644966604},"zoom":18},{"widget_id":"mini_map","name":"Third Floor","width":749,"height":270,"top":290,"left":119,"minimized":"block","extents":{"left":-77.056654993683,"bottom":38.891351677896,"right":-77.040154043823,"top":38.89757440281},"zoom":16},{"widget_id":"mini_calendar","name":"Upcoming Events","width":689,"height":200,"top":649,"left":224,"minimized":"none"}]}';
+    var config;
+    try {
+        config = JSON.parse(configString);
+    } catch (ex){
+        console.log("ERROR - Trying to parse config string, JSON error");
+        return;
+    }
+    if (config && config.widgets){
+        app.addWidgets(config.widgets);
+    } else {
+        console.log("ERROR - no widgets info when trying to load config data");
+    }
+};
+app.clearCookiesAndReload=function(){
+    app.saveConfig=false;
+    $.cookie('widget_config','{"status":"empty"}');
+    window.location.reload();
+};
+//=Helpers====================
+app.titleize=function(name){
+    return name.replace(/[ -0-9]/g,"");
+};
+app.getOpacity=function(elem) {
+    var ori = $(elem).css('opacity');
+    var ori2 = $(elem).css('filter');
+    if (ori2) {
+        ori2 = parseInt( ori2.replace(')','').replace('alpha(opacity=','') ) / 100;
+        if (!isNaN(ori2) && ori2 != '') {
+            ori = ori2;
+        }
+    }
+    return ori;
 };
